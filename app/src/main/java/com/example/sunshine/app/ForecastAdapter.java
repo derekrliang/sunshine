79da2b13d9514cbb7256879ffd1cd879ee0ccf5a
Derek Liang <frostyrusty@gmail.com>
2016-01-24 23:38:08 -0800

    added multiple item view types in forecast adapter

--- app/src/main/java/com/example/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/sunshine/app/ForecastAdapter.java
@@ -3,6 +3,7 @@ package com.example.sunshine.app;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +17,48 @@ import com.example.sunshine.app.data.WeatherContract;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+    private static final String TAG = ForecastAdapter.class.getSimpleName();
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
+    }
+
+
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
+    /**
+     * Copy/paste note: Replace existing newView() method in ForecastAdapter with this one.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        switch (viewType) {
+            case VIEW_TYPE_TODAY:
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            case VIEW_TYPE_FUTURE_DAY:
+                layoutId = R.layout.list_item_forecast;
+                break;
+            default:
+                Log.d(TAG, "Error, invalid layout for list item!");
+        }
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /**
@@ -42,17 +83,6 @@ public class ForecastAdapter extends CursorAdapter {
                 " - " + cursor.getString(MainActivityFragment.COL_WEATHER_DESC) +
                 " - " + highAndLow;
     }
-
-    /*
-        Remember that these views are reused as needed.
-     */
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-        return view;
-    }
-
     /*
         This is where we fill-in the views with the contents of the cursor.
      */
@@ -69,9 +99,9 @@ public class ForecastAdapter extends CursorAdapter {
         //iconView.setImageResource(R.drawable.ic_launcher);
         iconView.setImageResource(R.mipmap.ic_launcher);
 
-        Long date = cursor.getLong(MainActivityFragment.COL_WEATHER_DATE);
+        Long dateInMillis = cursor.getLong(MainActivityFragment.COL_WEATHER_DATE);
         TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
-        dateView.setText(Utility.getFriendlyDayString(context, date));
+        dateView.setText(Utility.getFriendlyDayString(context, dateInMillis));
 
         String forecast = cursor.getString(MainActivityFragment.COL_WEATHER_DESC);
         TextView forecastView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
